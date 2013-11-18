e635653d27b2d232e723ad5dcafae384668af49a
Boris Brinza <boris.brinza@gmail.com>
2013-11-18 17:07:13 +0100

    colors + comments

--- check.py
+++ check.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python
+
+# check dataset links at data.gov.sk
 import urllib2
 import urllib
 import json
 import os
 
-# Make the HTTP request.
+#some colors
+OK = '\033[92m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+
+#make the HTTP request
 response = urllib2.urlopen('http://data.gov.sk/api/1/rest/dataset')
 assert response.code == 200
         
-# Use the json module to load CKAN's response into a dictionary.
+
+#use the json module to load CKAN's response into a dictionary.
 response_dict = json.loads(response.read())
         
-
+#for all datasets get url, load metadata and check with curl
 for ds_name in response_dict:
   response = urllib2.urlopen('http://data.gov.sk/api/1/rest/dataset/' + ds_name)
   ds_metadata = json.loads(response.read())
   url =  ds_metadata['resources'][0]['url']
     
   ret = os.system("curl -X GET --output /dev/null --silent --head --fail \"" + url + "\"")
-  errorCode = (ret & int('0xff00', 16)) >> 8
+  errorCode = (ret & int('0xff00', 16)) >> 8	#special return code from os.system, see python docs
     
+  #print msg
   if (errorCode <>0):
-    print ("Chyba pre dataset:" + ds_name + ", url=" + url + ", kod chyby:" + str(errorCode))
+    print (FAIL + "ERR (" + str(errorCode) + ")" + ENDC + " pre dataset:" + ds_name + ", url=" + url)
   else:
-    print ("OK pre dataset:" + ds_name)
+    print (OK + "OK" + ENDC + " pre dataset:" + ds_name)
